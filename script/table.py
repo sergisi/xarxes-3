2a71762cceaded4b26a08f8f78d9071b07df835a
sergi <sergisiba@gmail.com>
2019-06-02 00:09:03 +0200

    Acabat nets i grafic

--- script/table.py
+++ script/table.py
@@ -2,7 +2,7 @@ import sys
 with open(sys.argv[1], 'r') as f:
     print('\\begin{table}[!h]\n\\centering\n\\begin{tabular}{|c|c|c|}\n\\hline')
     for line in f:
-        camps = line.split('&')
+        camps = line.split()
         print(' &'.join(camps), end='\\\\\n')
         print('\\hline')
     print('\\end{tabular}\n\\caption{·}\n\\label{·}\n\\end{table}')
